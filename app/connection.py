9467ec67248f39fdced43d3e06e3f2ee1f63fe83
sh_awtylol4 <97911154+suhun96@users.noreply.github.com>
2023-01-05 15:00:33 +0900

    Update connection.py

--- app/connection.py
+++ app/connection.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine("mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}".format(
-    DB_USERNAME = 'Hoon',
-    DB_PASSWORD = '9292455z',
+    DB_USERNAME = '',
+    DB_PASSWORD = '',
     DB_HOST = "127.0.0.1",
     DB_PORT = '3306',
     DB_DATABASE = 'FastAPI',
